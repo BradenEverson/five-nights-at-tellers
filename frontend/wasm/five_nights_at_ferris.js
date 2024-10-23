37cc55f546c9937ac8e44c36980136867519e201
Braden Everson <bradeneverson@gmail.com>
2024-10-23 08:37:28 -0500

    Create frontend

---
+++ frontend/wasm/five_nights_at_ferris.js
@@ -0,0 +1,4 @@
+import * as wasm from "./five_nights_at_ferris_bg.wasm";
+export * from "./five_nights_at_ferris_bg.js";
+import { __wbg_set_wasm } from "./five_nights_at_ferris_bg.js";
+__wbg_set_wasm(wasm);
