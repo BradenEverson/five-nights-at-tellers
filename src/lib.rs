6889773f80240ed3d00ebb7050a9524b59dd091f
BradenEverson <bradeneverson@gmail.com>
2024-10-23 05:03:13 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! The Game State Machine Definition, Creates a Game Session with a number of enemies, a target
+//! "time" to survive to, how many ticks create this time, and current door metadata
+
+/// The game's internal state, responsible for keeping track of what enemies we have, where they
+/// are, if our doors are closed, what time it is, etc!
+pub struct GameState {}
