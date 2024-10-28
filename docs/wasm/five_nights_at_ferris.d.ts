4c8d6bb9caeeb965d4776c0abf319a9117b171d2
BradenEverson <bradeneverson@gmail.com>
2024-10-28 13:39:56 -0500

    Move wasm

---
+++ docs/wasm/five_nights_at_ferris.d.ts
@@ -0,0 +1,131 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ *r" An enemy's ID for usage in the room HashMap
+ */
+export class EnemyId {
+  free(): void;
+}
+/**
+ * The full driver for a game responsible for holding both the enemies and the game state
+ */
+export class Game {
+  free(): void;
+  /**
+   * Ticks the game forward
+   * @returns {boolean}
+   */
+  tick(): boolean;
+  /**
+   * Gets the map context as a JsValue
+   * @returns {any}
+   */
+  get_map(): any;
+  /**
+   * Create a new game (trait impls aren't accessible to wasm_bindgen
+   * @returns {Game}
+   */
+  static new(): Game;
+  /**
+   * Gets the current time as an hour
+   * @returns {number}
+   */
+  get_time(): number;
+  /**
+   * Gets a snapshot of what the current camera room's name is and what enemies are in it
+   * @param {bigint} room
+   * @returns {(string)[] | undefined}
+   */
+  get_room(room: bigint): (string)[] | undefined;
+  /**
+   * Toggles the camera state
+   */
+  toggle_cameras(): void;
+  /**
+   * Close the left door
+   */
+  toggle_left(): void;
+  /**
+   * Close the right door
+   */
+  toggle_right(): void;
+  /**
+   * Is left door closed?
+   * @returns {boolean}
+   */
+  is_left_closed(): boolean;
+  /**
+   * Is right door closed?
+   * @returns {boolean}
+   */
+  is_right_closed(): boolean;
+  /**
+   * Check the current power draw
+   * @returns {number}
+   */
+  power_percent(): number;
+  /**
+   * Check if we're dead
+   * @returns {string | undefined}
+   */
+  is_dead(): string | undefined;
+  /**
+   * Render the current map
+   * @returns {string}
+   */
+  render(): string;
+}
+/**
+ *r" A room's ID
+ */
+export class RoomId {
+  free(): void;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_roomid_free: (a: number, b: number) => void;
+  readonly __wbg_game_free: (a: number, b: number) => void;
+  readonly game_tick: (a: number) => number;
+  readonly game_get_map: (a: number) => number;
+  readonly game_new: () => number;
+  readonly game_get_time: (a: number) => number;
+  readonly game_get_room: (a: number, b: number, c: number) => void;
+  readonly game_toggle_cameras: (a: number) => void;
+  readonly game_toggle_left: (a: number) => void;
+  readonly game_toggle_right: (a: number) => void;
+  readonly game_is_left_closed: (a: number) => number;
+  readonly game_is_right_closed: (a: number) => number;
+  readonly game_power_percent: (a: number) => number;
+  readonly game_is_dead: (a: number, b: number) => void;
+  readonly game_render: (a: number, b: number) => void;
+  readonly __wbg_enemyid_free: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
